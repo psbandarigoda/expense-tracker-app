d6836944fdc54581ca704f687bc82e3d962d211c
ifs-pasblk <pasindu.bandarigoda@ifs.com>
2022-12-29 22:48:16 +0530

    created app

---
+++ src/Route.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Expenses from './app/expenses/expenses';
+import { Route } from 'react-router-dom';
+
+const Route = () => {
+    <Route path="/expenses" component={Expenses} />
+};
+
+export default Route;
---
+++ src/app/expenses/expenses.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Expenses = () => {
+  return (
+    <div>
+      <h1>Expenses</h1>
+      <p>This is the Expenses page</p>
+    </div>
+  );
+};
+
+export default Expenses;
